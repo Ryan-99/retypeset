56c757d8d84886111a3915d9457a888901a5b7b1
Ryan-99 <73212341+Ryan-99@users.noreply.github.com>
2025-04-12 12:24:27 +0800

    Update ui.ts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,7 +1,7 @@
 export const ui = {
   'zh': {
-    title: '重新编排',
-    subtitle: '再现版式之美',
+    title: '回响',
+    subtitle: '一切热爱，终有回响',
     description: 'Retypeset是一款基于Astro框架的静态博客主题，中文名为重新编排。本主题以活版印字为设计灵感，通过建立全新的视觉规范，对所有页面进行重新编排，打造纸质书页般的阅读体验，再现版式之美。所见皆为细节，方寸尽显优雅。',
     posts: '文章',
     tags: '标签',
@@ -27,8 +27,8 @@ export const ui = {
     toc: '目次',
   },
   'en': {
-    title: 'Retypeset',
-    subtitle: 'Revive the beauty of typography',
+    title: 'Echoly',
+    subtitle: 'All passions will eventually find their echo',
     description: 'Retypeset is a static blog theme based on the Astro framework. Inspired by Typography, Retypeset establishes a new visual standard and reimagines the layout of all pages, creating a reading experience reminiscent of paper books, reviving the beauty of typography. Details in every sight, elegance in every space.',
     posts: 'Posts',
     tags: 'Tags',
